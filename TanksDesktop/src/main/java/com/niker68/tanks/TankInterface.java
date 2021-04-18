03fdd7c2d4d51a9a87edee65354ffd4d331256d9
niker68 <61888868+niker68@users.noreply.github.com>
2021-04-18 16:30:31 +0500

    final version

--- TanksDesktop/src/main/java/com/niker68/tanks/TankInterface.java
+++
@@ -1,9 +0,0 @@
-package com.niker68.tanks;
-
-public interface TankInterface {
-    boolean MoveUp();
-    boolean MoveDown();
-    boolean MoveRight();
-    boolean MoveLeft();
-    void shoot();
-}
